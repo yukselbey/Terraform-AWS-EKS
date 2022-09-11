3c7920dcfc34018927f7f9908c7a607fcdb89858
yuksel bey <yukselbey35@gmail.com>
2022-09-11 10:23:41 -0700

    create an EC2 with user data file

--- Terraform-Settings-Provider/provider.tf
+++ Terraform-Settings-Provider/provider.tf
@@ -20,12 +20,3 @@ desktop terminal $HOME/.aws/credentials
 Verify AWS Credentials 
 cat $HOME/.aws/credentials
 */
-# Resource: EC2 Instance
-resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
-  instance_type = "t3.micro"
-  user_data = file("${path.module}/app1-install.sh")
-  tags = {
-    "Name" = "EC2 Demo"
-  }
-}
